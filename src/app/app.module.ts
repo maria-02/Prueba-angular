744ed6d7cb14fb95736611fb9d6f188b298f67e6
maria-02 <69777624+maria-02@users.noreply.github.com>
2022-08-13 13:23:11 -0300

    agregado de estructuras (vacías)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MoviesListComponent } from './Movies/movies-list/movies-list.component';
+import { MoviesCardComponent } from './movies/components/movies-card/movies-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesListComponent,
+    MoviesCardComponent
   ],
   imports: [
     BrowserModule,
